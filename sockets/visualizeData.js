78d10822919b9f224f41452fc563f999886601f0
Muhammad Salahuddin <msalahuddin8890@gmail.com>
2020-08-30 10:08:19 -0700

    third party api

--- sockets/visualizeData.js
+++ sockets/visualizeData.js
@@ -1,12 +1,19 @@
-const { visualizeData } = require("../Models/data.model");
+const fetch = require('node-fetch')
 const moment = require("moment")
-module.exports = function (socket) {
+module.exports = async function (socket) {
   console.log("hello new file");
   let Open = [];
   let High = [];
   let Low = [];
   let Close = [];
   let Volume = [];
+ let data = await fetch('http://api.marketstack.com/v1/eod?access_key=e96e230697ccbb66d51c1146093c19ab&symbols=AAPL', {
+  method: 'GET',
+  headers: {'Content-Type': 'application/json'},
+}).then(response => {
+  return response.json();
+}).catch(err => {console.log(err);});
+console.log(data.data[0],'dddddddddddd')
     setInterval(async ()=>{
       let date = Date.now();
       Open.push({x: date, y: Math.floor((Math.random() * 3121231))});
